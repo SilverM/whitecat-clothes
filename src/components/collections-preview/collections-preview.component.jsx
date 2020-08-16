83348ede1e584258c66ee36eea258c363ac6fcd3
SilverM <SilverM@users.noreply.github.com>
2020-08-16 23:37:29 +0800

    working version

--- src/components/collections-preview/collections-preview.component.jsx
+++ src/components/collections-preview/collections-preview.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './collections-preview.styles.scss';
 import CollectionItem from '../collection-item/collection-item.component';
 
-const CollectionPreview = ({title, items}) => (
+export const CollectionPreview = ({title, items}) => (
     <div className = 'collection-preview'>
         <h1 className = 'title'>{title.toUpperCase()}</h1>
         <div className = 'preview'>
@@ -17,4 +17,3 @@ const CollectionPreview = ({title, items}) => (
     </div>
 );
 
-export default CollectionPreview
